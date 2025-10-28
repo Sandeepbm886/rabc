2e986c9217d3cb994a70fd6dbf95ef52774f5869
Sandeep <sandeepbm886@gmail.com>
2025-10-28 23:38:08 +0530

    fixed videocall name and logo in layout

--- app/videocall/(root)/layout.jsx
+++ app/videocall/(root)/layout.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import StreamVideoProvider from '../providers/StreamClientProvider';
-export const metadata = {
-  title: "YOOM",
-  description: "Video calling App",
-  icons: {
-    icon: "/icons/logo.svg",
-  },
-};
+
 function RootLayout({children}) {
   return (
     <main>
